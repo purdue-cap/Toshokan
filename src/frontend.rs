66d08aba3d409e1b6f89ee411e468820ba9fb947
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-04-08 19:29:12 -0400

    Module structures

--- src/frontend.rs
+++ src/frontend.rs
@@ -1,5 +1,16 @@
 pub mod parser;
+
 mod sketch_runner;
 pub use sketch_runner::SketchRunner;
 pub use sketch_runner::VerificationResult;
 pub use sketch_runner::SynthesisResult;
+pub use sketch_runner::GenerationResult;
+
+mod cand_encoder;
+pub use cand_encoder::CandEncoder;
+mod ce_encoder;
+pub use ce_encoder::CEEncoder;
+mod encode_error;
+pub use encode_error::EncodeError;
+
+pub mod template_helpers;
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,11 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+extern crate clang;
+
+#[macro_use]
+extern crate quick_error;
+
+pub mod backend;
 pub mod frontend;
+pub mod cegis;

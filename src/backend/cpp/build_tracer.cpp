c84f6e7d8790c5569d6d6a4a681e2f68caff8927
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-04-10 12:11:44 -0400

    C++ BuildTracer

--- src/backend/cpp/build_tracer.cpp
+++ src/backend/cpp/build_tracer.cpp
@@ -1,3 +1,8 @@
+#include <string>
+#include <sstream>
+#include <vector>
+#include <system_error>
+
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
@@ -5,6 +10,7 @@
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "clang/Tooling/Tooling.h"
+#include "clang/Tooling/CompilationDatabase.h"
 
 #ifdef TESTBIN
 #include "clang/Tooling/CommonOptionsParser.h"
@@ -15,21 +21,96 @@ namespace TracerBuilder {
 using namespace clang;
 using namespace clang::tooling;
 
+using std::string;
+using std::stringstream;
+using std::vector;
+
 #ifdef TESTBIN
 static llvm::cl::OptionCategory TestBinCategory("Test Binary");
 #endif // TESTBIN
 
 class TracerBuilderVisitor : public RecursiveASTVisitor<TracerBuilderVisitor> {
     public:
-    TracerBuilderVisitor(Rewriter &R) : TheRewriter(R) {}
+    TracerBuilderVisitor(Rewriter &R, string &name) : TheRewriter(R), LibFuncName(name) {}
+
+    bool VisitFunctionDecl(FunctionDecl *f) {
+        auto &SM = TheRewriter.getSourceMgr();
+        if (f->hasBody()
+            && f->getNameAsString() == LibFuncName
+            && SM.isInMainFile(f->getBeginLoc())) {
+            // Construct impl function name
+            stringstream impl_name;
+            impl_name << LibFuncName << "_impl";
+
+            // Build actual parameter list
+            stringstream arg_list;
+            for (unsigned i = 0; i < f->getNumParams() - 1; i++) {
+                arg_list << f->getParamDecl(i)->getName().str();
+                if (i != f->getNumParams() - 2) {
+                    arg_list << ", ";
+                }
+            }
+
+            // Build impl function call
+            stringstream impl_call;
+            impl_call << impl_name.str() << "(" << arg_list.str() << ")";
+
+            // Get the returning paramter
+            const auto* rtn_param = f->getParamDecl(f->getNumParams() - 1);
+
+            // Construct log str
+            stringstream log_str;
+            log_str << '"' << LibFuncName << "(";
+            for (unsigned i = 0; i < f->getNumParams() - 1; i++) {
+                log_str << "%d"; // TODO: Support more types than integer here
+                if (i != f->getNumParams() - 2) {
+                    log_str << ", ";
+                }
+            }
+            log_str << ") = %d\\n" << '"';
+
+            // Construct log statement
+            stringstream log_statement;
+            log_statement << "fprintf(stderr, " << log_str.str() << ", "
+            << arg_list.str() << ", " << rtn_param->getName().str() << ")";
+
+
+            // Build Body
+            stringstream body;
+            body << "{\n"
+            << "  " << rtn_param->getName().str() << " = " << impl_call.str() << ";\n"
+            << "  " << log_statement.str() << ";\n"
+            << "}";
+
+            // Construct declaration
+            stringstream decl;
+            decl << rtn_param->getType().getNonReferenceType().getAsString()
+            << " " << impl_name.str() << "(";
+            for (unsigned i = 0; i < f->getNumParams() - 1; i++) {
+                decl << f->getParamDecl(i)->getType().getAsString();
+                if (i != f->getNumParams() - 2) {
+                    decl << ", ";
+                }
+            }
+            decl << ");\n";
+
+
+            auto range = f->getBody()->getSourceRange();
+            TheRewriter.ReplaceText(range, body.str());
+            auto startOfFile = SM.getLocForStartOfFile(SM.getMainFileID());
+            TheRewriter.InsertText(startOfFile, decl.str());
+        }
+        return true;
+    }
 
     private:
     Rewriter &TheRewriter;
+    string &LibFuncName;
 };
 
 class TracerBuilderASTConsumer : public ASTConsumer {
     public:
-    TracerBuilderASTConsumer(Rewriter &R) : Visitor(R) {}
+    TracerBuilderASTConsumer(Rewriter &R, string &name) : Visitor(R, name) {}
 
     // Override the method that gets called for each parsed top-level
     // declaration.
@@ -47,27 +128,61 @@ class TracerBuilderASTConsumer : public ASTConsumer {
 
 class TracerBuilderFrontendAction : public ASTFrontendAction {
     public:
-    TracerBuilderFrontendAction() {}
+    TracerBuilderFrontendAction(string &name, llvm::raw_ostream &out): LibFuncName(name), OutStream(out) {}
 
     void EndSourceFileAction() override {
         SourceManager &SM = TheRewriter.getSourceMgr();
-        llvm::errs() << "** EndSourceFileAction for: "
-                    << SM.getFileEntryForID(SM.getMainFileID())->getName() << "\n";
 
         // Now emit the rewritten buffer.
-        TheRewriter.getEditBuffer(SM.getMainFileID()).write(llvm::outs());
+        TheRewriter.getEditBuffer(SM.getMainFileID()).write(OutStream);
     }
 
     std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                     StringRef file) override {
-        llvm::errs() << "** Creating AST consumer for: " << file << "\n";
         TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-        return llvm::make_unique<TracerBuilderASTConsumer>(TheRewriter);
+        return llvm::make_unique<TracerBuilderASTConsumer>(TheRewriter, LibFuncName);
     }
 
     private:
     Rewriter TheRewriter;
+    string &LibFuncName;
+    llvm::raw_ostream &OutStream;
 };
+
+class TracerBuilderFrontendActionFactory : public FrontendActionFactory {
+    public:
+    TracerBuilderFrontendActionFactory(string &name, llvm::raw_ostream &out) : LibFuncName(name), OutStream(out) {}
+
+    FrontendAction* create() {
+        return new TracerBuilderFrontendAction(LibFuncName, OutStream);
+    }
+
+    private:
+    string LibFuncName;
+    llvm::raw_ostream &OutStream;
+};
+
+int BuildTracer(string& lib_func_name, string& input_file, llvm::raw_ostream &out) {
+    string source_path_list[1] = {input_file};
+    auto dir_path = llvm::sys::path::parent_path(input_file);
+    string error_msg;
+    auto db = CompilationDatabase::loadFromDirectory(dir_path, error_msg);
+    if (db == nullptr) {
+        return 1;
+    }
+    ClangTool tool(*db, source_path_list);
+    TracerBuilderFrontendActionFactory factory(lib_func_name, out);
+    return tool.run(&factory);
+}
+
+int BuildTracer(string& lib_func_name, string& input_file, string& output_file) {
+    std::error_code EC;
+    llvm::raw_fd_ostream out_stream(output_file, EC);
+    if (EC) {
+        return EC.value();
+    }
+    return BuildTracer(lib_func_name, input_file, out_stream);
+}
 
 } // namespace TracerBuilder
 
@@ -75,9 +190,18 @@ class TracerBuilderFrontendAction : public ASTFrontendAction {
 using namespace TracerBuilder;
 
 int main(int argc, const char **argv) {
-    CommonOptionsParser op(argc, argv, TestBinCategory);
-    ClangTool tool(op.getCompilations(), op.getSourcePathList());
+    if (argc < 3) {
+        return 1;
+    }
+    string input_file(argv[1]);
+    string func_name(argv[2]);
 
-    return tool.run(newFrontendActionFactory<TracerBuilderFrontendAction>().get());
+    if (argc >= 4) {
+        string output_file(argv[3]);
+        return BuildTracer(func_name, input_file, output_file);
+    } else {
+        return BuildTracer(func_name, input_file, llvm::outs());
+    }
+
 }
 #endif // TESTBIN

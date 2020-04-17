e0046e8b1aeb5087b5219906422cae55ed73e1bc
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-04-17 14:29:30 -0400

    Use impl to do verification

--- examples/primality.rs
+++ examples/primality.rs
@@ -1,17 +1,21 @@
 extern crate libpartlibspec;
-use libpartlibspec::cegis::{CEGISConfig, CEGISLoop};
+use libpartlibspec::cegis::{CEGISConfig, CEGISLoop, VerifyPointsConfig};
 use std::path::PathBuf;
+use std::collections::HashSet;
 use simplelog::{TermLogger, LevelFilter, Config, TerminalMode};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     TermLogger::init(LevelFilter::Debug, Config::default(), TerminalMode::Mixed)?;
     let base_data_dir = PathBuf::from(file!()).parent().ok_or("Get parent failed")?.join("data/primality");
-    let verification = base_data_dir.join("verificationMain.sk");
+    let verification = base_data_dir.join("verificationMainImpl.sk");
     let synthesis = base_data_dir.join("synthesisMain.sk");
     let generation = base_data_dir.join("generationMain.sk");
     let impl_file = base_data_dir.join("impl.cpp");
     let sketch_bin = PathBuf::from("sketchsynth");
     let sketch_home = PathBuf::from("/usr/share/sketchsynth/runtime/");
+    let v_p_set : HashSet<_> = (0..8).map(
+        |n| vec![n]
+    ).collect();
     let config = CEGISConfig::new(
         sketch_bin.as_path(),
         sketch_home.as_path(),
@@ -19,6 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "sqrt",
         1,
         1,
+        VerifyPointsConfig::Fixed(v_p_set),
         10,
         7,
         1,
@@ -26,12 +31,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         verification.as_path(),
         synthesis.as_path(),
         generation.as_path(),
-        &["p_3_6_0"]);
+        &["p_1_1_0"]);
     let mut main_loop = CEGISLoop::new(config);
 
-    for v_p in [1, 2, 6, 23, 29, 7, 16, 148].iter() {
-        main_loop.get_state_mut().add_verify_point(vec![*v_p]);
-    }
     println!("{}", main_loop.run_loop()?.or(Some("Unsolvable benchmark".to_string())).unwrap());
     Ok(())
 }

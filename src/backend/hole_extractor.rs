e6aa2151d57edf1248c3f60f732bccca47c8e54a
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-05-18 23:41:08 -0400

    Modify hole extractor to extract holes with name

--- src/backend/hole_extractor.rs
+++ src/backend/hole_extractor.rs
@@ -2,23 +2,33 @@ use quick_xml::{Reader, Error};
 use quick_xml::events::{Event, BytesStart};
 use std::io::BufRead;
 use std::path::Path;
-use std::iter::repeat;
+use regex::Regex;
 use std::collections::HashMap;
 
 pub struct HoleExtractor {
-    n_holes: usize,
     hole_offset: usize
 }
 
 impl HoleExtractor {
-    pub fn new(n_holes: usize, hole_offset: usize) -> Self {
+    pub fn new(hole_offset: usize) -> Self {
         HoleExtractor {
-            n_holes: n_holes,
             hole_offset: hole_offset
         }
     }
 
-    fn extract_hole_from_element(&self, holes: &mut Vec<Option<isize>>, element: &BytesStart) -> Option<()> {
+    fn map_hole_name<S: AsRef<str>>(&self, name: S) -> Option<String> {
+        let hole_regex = Regex::new(r"H__(\d+)([\d_]*)").expect("Hard coded regex should not fail");
+        let caps = hole_regex.captures(name.as_ref())?;
+        let hole_index = caps.get(1)?.as_str().parse::<usize>().ok()?;
+        if hole_index < self.hole_offset {
+            None
+        } else {
+            Some(format!("H__{}{}", hole_index - self.hole_offset, caps.get(2)?.as_str()))
+        }
+
+    }
+
+    fn extract_hole_from_element(&self, holes: &mut HashMap<String, isize>, element: &BytesStart) -> Option<()> {
         if element.name() != b"hole_value" { return None; }
         let attrs: HashMap<_, _>= element.attributes()
                     .into_iter().flatten()
@@ -28,21 +38,18 @@ impl HoleExtractor {
         if e_type.as_ref() != b"int" { return None; }
 
         let e_name = std::str::from_utf8(attrs.get(b"name".as_ref())?).ok()?;
-        let e_name_parts: Vec<_> = e_name.split("__").collect();
-        if e_name_parts.get(0)? != &"H" { return None; }
-        let hole_index = e_name_parts.get(1)?.parse::<usize>().ok()?;
-        if hole_index < self.hole_offset { return None; }
+        let h_name = self.map_hole_name(&e_name)?;
 
         let e_value = std::str::from_utf8(attrs.get(b"value".as_ref())?).ok()?
                         .parse::<isize>().ok()?;
         
-        *holes.get_mut(hole_index - self.hole_offset)? = Some(e_value);
+        holes.insert(h_name, e_value);
         Some(())
     }
 
-    pub fn read_holes<B: BufRead>(&self, r: &mut Reader<B>) -> Result<Vec<isize>, Error> {
+    pub fn read_holes<B: BufRead>(&self, r: &mut Reader<B>) -> Result<HashMap<String, isize>, Error> {
         let mut buffer = Vec::new();
-        let mut holes: Vec<Option<isize>> = repeat(None).take(self.n_holes).collect();
+        let mut holes = HashMap::<String, isize>::new();
         loop {
             match r.read_event(&mut buffer)? {
                 Event::Start(ref element) | Event::Empty(ref element) => {
@@ -53,15 +60,15 @@ impl HoleExtractor {
             }
         }
         buffer.clear();
-        holes.into_iter().collect::<Option<_>>().ok_or(quick_xml::Error::TextNotFound)
+        Ok(holes)
     }
 
-    pub fn read_holes_from_str<S: AsRef<str>>(&self, xml: S) -> Result<Vec<isize>, Error> {
+    pub fn read_holes_from_str<S: AsRef<str>>(&self, xml: S) -> Result<HashMap<String, isize>, Error> {
         let mut reader = Reader::from_str(xml.as_ref());
         self.read_holes(&mut reader)
     }
 
-    pub fn read_holes_from_file<P: AsRef<Path>>(&self, file_name: P) -> Result<Vec<isize>, Error> {
+    pub fn read_holes_from_file<P: AsRef<Path>>(&self, file_name: P) -> Result<HashMap<String, isize>, Error> {
         let mut reader = Reader::from_file(file_name)?;
         self.read_holes(&mut reader)
     }
@@ -77,11 +84,31 @@ mod tests {
         let xml_str = r#"
         <?xml version="1.0"?>
         <hole_values>
-            <hole_value line="45" col="-1"  name="H__6" type="int" value="1" />
-            <hole_value line="9" col="-1"  name="H__0" type="array">
+            <hole_value line="10" col="-1"  name="H__3_1_0_0_0_0_1_0_0_0_0" type="int" value="0" />
+            <hole_value line="4" col="-1"  name="H__1_1" type="int" value="0" />
+            <hole_value line="4" col="-1"  name="H__1_1_0_0_0" type="int" value="1" />
+            <hole_value line="8" col="-1"  name="H__5_1" type="int" value="1" />
+            <hole_value line="7" col="-1"  name="H__2_1_0" type="int" value="1" />
+            <hole_value line="7" col="-1"  name="H__2_1_0_0_0" type="int" value="1" />
+            <hole_value line="8" col="-1"  name="H__5_1_0_0_0" type="int" value="1" />
+            <hole_value line="5" col="-1"  name="H__4_1_0_0_0" type="int" value="1" />
+            <hole_value line="8" col="-1"  name="H__5_1_0_0_0_0" type="int" value="0" />
+            <hole_value line="4" col="-1"  name="H__1_1_0_0_0_0" type="int" value="1" />
+            <hole_value line="7" col="-1"  name="H__2_1" type="int" value="0" />
+            <hole_value line="10" col="-1"  name="H__3_1" type="int" value="1" />
+            <hole_value line="5" col="-1"  name="H__4_1_0" type="int" value="1" />
+            <hole_value line="7" col="-1"  name="H__2_1_0_0" type="int" value="1" />
+            <hole_value line="8" col="-1"  name="H__5_1_0" type="int" value="0" />
+            <hole_value line="10" col="-1"  name="H__3_1_0_0" type="int" value="1" />
+            <hole_value line="4" col="-1"  name="H__1_1_0_0" type="int" value="1" />
+            <hole_value line="5" col="-1"  name="H__4_1" type="int" value="0" />
+            <hole_value line="5" col="-1"  name="H__4_1_0_0" type="int" value="0" />
+            <hole_value line="4" col="-1"  name="H__1_1_0" type="int" value="1" />
+            <hole_value line="10" col="-1"  name="H__3_1_0_0_0" type="int" value="1" />
+            <hole_value line="1" col="-1"  name="H__0" type="array">
                 <entry value="0" />
                 <entry value="0" />
-                <entry value="0" />
+                <entry value="2" />
                 <entry value="0" />
                 <entry value="0" />
                 <entry value="0" />
@@ -90,17 +117,41 @@ mod tests {
                 <entry value="0" />
                 <entry value="0" />
             </hole_value>
-            <hole_value line="48" col="-1"  name="H__7" type="int" value="0" />
-            <hole_value line="29" col="-1"  name="H__1" type="int" value="1" />
-            <hole_value line="39" col="-1"  name="H__4" type="int" value="0" />
-            <hole_value line="30" col="-1"  name="H__2" type="int" value="0" />
-            <hole_value line="42" col="-1"  name="H__5" type="int" value="0" />
-            <hole_value line="32" col="-1"  name="H__3" type="int" value="0" />
+            <hole_value line="5" col="-1"  name="H__4_1_0_0_0_0" type="int" value="0" />
+            <hole_value line="10" col="-1"  name="H__3_1_0" type="int" value="1" />
+            <hole_value line="8" col="-1"  name="H__5_1_0_0" type="int" value="1" />
+            <hole_value line="7" col="-1"  name="H__2_1_0_0_0_0" type="int" value="0" />
         </hole_values>
         "#;
-        let extractor = HoleExtractor::new(7, 1);
+        let extractor = HoleExtractor::new(1);
         let holes = extractor.read_holes_from_str(&xml_str)?;
-        assert_eq!(holes, vec![1, 0, 0, 0, 0, 1, 0]);
+        let mut holes_fixture = HashMap::<String, isize>::new();
+        holes_fixture.insert("H__2_1_0_0_0_0_1_0_0_0_0".to_string(), 0);
+        holes_fixture.insert("H__0_1".to_string(), 0);
+        holes_fixture.insert("H__0_1_0_0_0".to_string(), 1);
+        holes_fixture.insert("H__4_1".to_string(), 1);
+        holes_fixture.insert("H__1_1_0".to_string(), 1);
+        holes_fixture.insert("H__1_1_0_0_0".to_string(), 1);
+        holes_fixture.insert("H__4_1_0_0_0".to_string(), 1);
+        holes_fixture.insert("H__3_1_0_0_0".to_string(), 1);
+        holes_fixture.insert("H__4_1_0_0_0_0".to_string(), 0);
+        holes_fixture.insert("H__0_1_0_0_0_0".to_string(), 1);
+        holes_fixture.insert("H__1_1".to_string(), 0);
+        holes_fixture.insert("H__2_1".to_string(), 1);
+        holes_fixture.insert("H__3_1_0".to_string(), 1);
+        holes_fixture.insert("H__1_1_0_0".to_string(), 1);
+        holes_fixture.insert("H__4_1_0".to_string(), 0);
+        holes_fixture.insert("H__2_1_0_0".to_string(), 1);
+        holes_fixture.insert("H__0_1_0_0".to_string(), 1);
+        holes_fixture.insert("H__3_1".to_string(), 0);
+        holes_fixture.insert("H__3_1_0_0".to_string(), 0);
+        holes_fixture.insert("H__0_1_0".to_string(), 1);
+        holes_fixture.insert("H__2_1_0_0_0".to_string(), 1);
+        holes_fixture.insert("H__3_1_0_0_0_0".to_string(), 0);
+        holes_fixture.insert("H__2_1_0".to_string(), 1);
+        holes_fixture.insert("H__4_1_0_0".to_string(), 1);
+        holes_fixture.insert("H__1_1_0_0_0_0".to_string(), 0);
+        assert_eq!(holes, holes_fixture);
         Ok(())
     }
 

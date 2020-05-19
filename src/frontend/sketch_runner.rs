d671a1bd5fb61dd0fd6e5e64c7f7127dcc5821d2
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-05-18 23:03:06 -0400

    Add backend runner to Sketch Runner

--- src/frontend/sketch_runner.rs
+++ src/frontend/sketch_runner.rs
@@ -6,9 +6,12 @@ use tempfile::NamedTempFile;
 use log::{debug, trace};
 
 pub struct SketchRunner {
-    path: OsString,
-    sketch: Command,
-    flags: Vec<OsString>,
+    frontend_path: OsString,
+    backend_path: OsString,
+    frontend_cmd: Command,
+    backend_cmd: Command,
+    fe_flags: Vec<OsString>,
+    be_flags: Vec<OsString>,
     output_dir: PathBuf
 }
 
@@ -24,69 +27,105 @@ pub enum SynthesisResult {
     ExecutionErr(io::Error)
 }
 
-pub type GenerationResult = io::Result<PathBuf>; // Generated base file name
+pub type GenerationResult = io::Result<(PathBuf, String)>; // Generated base file name
 
 impl SketchRunner{
-    pub fn new<P: AsRef<Path>>(path: P, output_dir: P) -> Self{
+    pub fn new<P: AsRef<Path>>(frontend_path: P, backend_path:P, output_dir: P) -> Self{
         SketchRunner{
-            path: path.as_ref().as_os_str().to_os_string(),
-            sketch: Command::new(path.as_ref().as_os_str()),
-            flags: Vec::<OsString>::new(),
+            frontend_path: frontend_path.as_ref().as_os_str().to_os_string(),
+            backend_path: backend_path.as_ref().as_os_str().to_os_string(),
+            frontend_cmd: Command::new(frontend_path.as_ref().as_os_str()),
+            backend_cmd: Command::new(backend_path.as_ref().as_os_str()),
+            fe_flags: Vec::<OsString>::new(),
+            be_flags: Vec::<OsString>::new(),
             output_dir: output_dir.as_ref().to_path_buf()
         }
     }
-    pub fn clear(&mut self) -> &mut Self {
-        self.flags.clear();
+    pub fn fe_clear(&mut self) -> &mut Self {
+        self.fe_flags.clear();
         self
     }
 
-    pub fn flag<S: AsRef<OsStr>>(&mut self, opt: S) -> &mut Self{
-        self.flags.push(opt.as_ref().to_os_string());
+    pub fn fe_flag<S: AsRef<OsStr>>(&mut self, opt: S) -> &mut Self{
+        self.fe_flags.push(opt.as_ref().to_os_string());
         self
     }
 
-    pub fn output<P: AsRef<Path>>(&mut self, input_file:P) -> io::Result<Output> {
-        self.sketch.args(&self.flags);
-        self.sketch.arg(input_file.as_ref());
-        debug!(target: "SketchRunner", "Sketch command: {:?}", self.sketch);
-        let result = self.sketch.output();
-        trace!(target: "SketchRunner", "Sketch result.status: {:?}", result.as_ref().ok().map(|r| r.status));
-        trace!(target: "SketchRunner", "Sketch result.stdout: {}",
+    pub fn be_clear(&mut self) -> &mut Self {
+        self.be_flags.clear();
+        self
+    }
+
+    pub fn be_flag<S: AsRef<OsStr>>(&mut self, opt: S) -> &mut Self{
+        self.be_flags.push(opt.as_ref().to_os_string());
+        self
+    }
+
+    pub fn fe_output<P: AsRef<Path>>(&mut self, input_file:P) -> io::Result<Output> {
+        self.frontend_cmd.args(&self.fe_flags);
+        self.frontend_cmd.arg(input_file.as_ref());
+        debug!(target: "SketchRunner", "Sketch Frontend command: {:?}", self.frontend_cmd);
+        let result = self.frontend_cmd.output();
+        trace!(target: "SketchRunner", "Sketch Frontend result.status: {:?}", result.as_ref().ok().map(|r| r.status));
+        trace!(target: "SketchRunner", "Sketch Frontend result.stdout: {}",
             result.as_ref().ok().map(|r| &r.stdout)
             .and_then(|b| String::from_utf8(b.clone()).ok())
             .unwrap_or("<Failure>".to_string())
         );
-        trace!(target: "SketchRunner", "Sketch result.stderr: {}",
+        trace!(target: "SketchRunner", "Sketch Frontend result.stderr: {}",
             result.as_ref().ok().map(|r| &r.stderr)
             .and_then(|b| String::from_utf8(b.clone()).ok())
             .unwrap_or("<Failure>".to_string())
         );
-        self.sketch = Command::new(self.path.clone());
+        self.frontend_cmd = Command::new(self.frontend_path.clone());
         result
     }
 
-    pub fn flag_verify(&mut self) -> &mut Self {
-        self.clear().flag("--debug-cex").flag("-V").flag("3")
+    pub fn be_output<P: AsRef<Path>>(&mut self, input_file:P) -> io::Result<Output> {
+        self.backend_cmd.args(&self.be_flags);
+        self.backend_cmd.arg(input_file.as_ref());
+        debug!(target: "SketchRunner", "Sketch Backend command: {:?}", self.backend_cmd);
+        let result = self.backend_cmd.output();
+        trace!(target: "SketchRunner", "Sketch Backend result.status: {:?}", result.as_ref().ok().map(|r| r.status));
+        trace!(target: "SketchRunner", "Sketch Backend result.stdout: {}",
+            result.as_ref().ok().map(|r| &r.stdout)
+            .and_then(|b| String::from_utf8(b.clone()).ok())
+            .unwrap_or("<Failure>".to_string())
+        );
+        trace!(target: "SketchRunner", "Sketch Backend result.stderr: {}",
+            result.as_ref().ok().map(|r| &r.stderr)
+            .and_then(|b| String::from_utf8(b.clone()).ok())
+            .unwrap_or("<Failure>".to_string())
+        );
+        self.backend_cmd = Command::new(self.backend_path.clone());
+        result
     }
 
-    pub fn flag_synthesize(&mut self) -> &mut Self {
-        let output_dir = self.output_dir.clone();
-        self.clear().flag("--fe-output-test")
-            .flag("--fe-output-dir").flag(output_dir.join("./"))
-            .flag("--fe-output-xml").flag(output_dir.join("holes.xml"))
-            .flag("--fe-output-hole-func").flag(output_dir.join("holes.txt"))
+    pub fn set_be_verify_flags(&mut self, flags: &Vec<OsString>) -> &mut Self {
+        self.be_clear();
+        for flag in flags {
+            self.be_flag(&flag);
+        }
+        self
     }
 
-    pub fn flag_generate(&mut self) -> &mut Self {
+    pub fn fe_flag_synthesize(&mut self) -> &mut Self {
         let output_dir = self.output_dir.clone();
-        self.clear().flag("--fe-output-test")
-            .flag("--fe-output-dir").flag(output_dir.join("./"))
-            .flag("--fe-force-codegen")
+        self.fe_clear().fe_flag("--fe-output-test")
+            .fe_flag("--fe-output-dir").fe_flag(output_dir.join("./"))
+            .fe_flag("--fe-output-xml").fe_flag(output_dir.join("holes.xml"))
+    }
+
+    pub fn fe_flag_generate(&mut self) -> &mut Self {
+        let output_dir = self.output_dir.clone();
+        self.fe_clear().fe_flag("--fe-keep-tmp")
+            .fe_flag("--fe-cegis-path").fe_flag("_sketch_dummy")
+            .fe_flag("--fe-tempdir").fe_flag(output_dir.join("./"))
+            .fe_flag("--debug-cex").fe_flag("-V").fe_flag("3")
     }
 
     pub fn verify_file<P: AsRef<Path>>(&mut self, input_file:P) -> VerificationResult {
-        self.flag_verify();
-        match self.output(input_file) {
+        match self.be_output(input_file) {
             Ok(output) => {
                 if let Some(code) = output.status.code() {
                     if code == 0 {
@@ -107,8 +146,8 @@ impl SketchRunner{
     }
 
     pub fn synthesize_file<P: AsRef<Path>>(&mut self, input_file:P) -> SynthesisResult {
-        self.flag_synthesize();
-        match self.output(input_file) {
+        self.fe_flag_synthesize();
+        match self.fe_output(input_file) {
             Ok(output) => {
                 if let Some(code) = output.status.code() {
                     if code == 0 {
@@ -125,9 +164,16 @@ impl SketchRunner{
     }
 
     pub fn generate_file<P: AsRef<Path>>(&mut self, input_file:P) -> GenerationResult {
-        self.flag_generate();
-        self.output(input_file.as_ref()).and(Ok(self.output_dir.join(input_file.as_ref().file_name()
-            .ok_or(io::Error::new(io::ErrorKind::InvalidInput, "Input file has no base file name"))?)))
+        self.fe_flag_generate();
+        self.fe_output(input_file.as_ref()).and_then(|output| {
+            let base_name = input_file.as_ref().file_name()
+                .ok_or(io::Error::new(io::ErrorKind::InvalidInput, "Input file has no base file name"))?;
+            let tmp_path = self.output_dir.join("tmp").join(base_name).join("input0.tmp");
+            match String::from_utf8(output.stdout) {
+                Ok(decoded) => Ok((tmp_path, decoded)),
+                Err(error) => Err(io::Error::new(io::ErrorKind::Other, error))
+            }
+        })
     }
 
     pub fn verify_str<S: AsRef<str>>(&mut self, input: S) -> VerificationResult {

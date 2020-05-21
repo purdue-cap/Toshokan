56a0f4db1fe98b68f3b3a16eb979ef8b5b509c22
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-05-21 05:31:46 -0400

    runnable primality example

--- examples/primality.rs
+++ examples/primality.rs
@@ -1,7 +1,6 @@
 extern crate libpartlibspec;
 use libpartlibspec::cegis::{CEGISConfig, CEGISLoop, VerifyPointsConfig};
 use std::path::PathBuf;
-use std::collections::HashSet;
 use std::fs::File;
 use simplelog::{TermLogger, LevelFilter, Config, TerminalMode};
 
@@ -12,29 +11,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     TermLogger::init(log_level, Config::default(), TerminalMode::Mixed)?;
     let base_data_dir = PathBuf::from(file!()).parent().ok_or("Get parent failed")?.join("data/primality");
-    let verification = base_data_dir.join("verificationMain.sk");
     let synthesis = base_data_dir.join("synthesisMain.sk");
-    let generation = base_data_dir.join("generationMain.sk");
+    let verification = base_data_dir.join("verificationMain.sk");
     let impl_file = base_data_dir.join("impl.cpp");
-    let sketch_bin = PathBuf::from("sketchsynth");
+    let sketch_fe_bin = PathBuf::from("sketchsynth");
+    let sketch_be_bin = PathBuf::from("/usr/share/sketchsynth/bin/cegis");
     let sketch_home = PathBuf::from("/usr/share/sketchsynth/runtime/");
     let config = CEGISConfig::new(
-        sketch_bin.as_path(),
+        sketch_fe_bin.as_path(),
+        sketch_be_bin.as_path(),
         sketch_home.as_path(),
         impl_file.as_path(),
         "sqrt",
         "main",
         1,
         1,
-        VerifyPointsConfig::Fixed(HashSet::new()),
+        VerifyPointsConfig::NoSpec,
         10,
-        4,
         1,
         true,
         true,
-        verification.as_path(),
         synthesis.as_path(),
-        generation.as_path(),
+        verification.as_path(),
         &["p_1_1_0"]);
     let mut main_loop = CEGISLoop::new(config);
 

94a5fd3c11bb8b87ff923d69ec7b3767ccb6240a
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-06-04 02:16:21 -0400

    powerroot adaption for the latest version

--- examples/powerroot.rs
+++ examples/powerroot.rs
@@ -1,7 +1,6 @@
 extern crate libpartlibspec;
 use libpartlibspec::cegis::{CEGISConfig, CEGISLoop, VerifyPointsConfig};
 use std::path::PathBuf;
-use std::collections::HashSet;
 use std::fs::File;
 use simplelog::{TermLogger, LevelFilter, Config, TerminalMode};
 
@@ -12,29 +11,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     TermLogger::init(log_level, Config::default(), TerminalMode::Mixed)?;
     let base_data_dir = PathBuf::from(file!()).parent().ok_or("Get parent failed")?.join("data/powerroot");
-    let verification = base_data_dir.join("verificationMain.sk");
     let synthesis = base_data_dir.join("synthesisMain.sk");
-    let generation = base_data_dir.join("generationMain.sk");
+    let verification = base_data_dir.join("verificationMain.sk");
     let impl_file = base_data_dir.join("impl.cpp");
-    let sketch_bin = PathBuf::from("sketchsynth");
-    let sketch_home = PathBuf::from("/usr/share/sketchsynth/runtime/");
+    let mut sketch_fe_bin = PathBuf::from("sketchsynth");
+    if let Ok(env_path) = std::env::var("SKETCH_FE") {
+        sketch_fe_bin = PathBuf::from(env_path);
+    }
+    let mut sketch_be_bin = PathBuf::from("/usr/share/sketchsynth/bin/cegis");
+    if let Ok(env_path) = std::env::var("SKETCH_BE") {
+        sketch_be_bin = PathBuf::from(env_path);
+    }
+    let mut sketch_home = PathBuf::from("/usr/share/sketchsynth/runtime/");
+    if let Ok(env_path) = std::env::var("SKETCH_HOME") {
+        sketch_home = PathBuf::from(env_path);
+    }
     let config = CEGISConfig::new(
-        sketch_bin.as_path(),
+        sketch_fe_bin.as_path(),
+        sketch_be_bin.as_path(),
         sketch_home.as_path(),
         impl_file.as_path(),
         "sqrt",
         "main",
         1,
         1,
-        VerifyPointsConfig::Fixed(HashSet::new()),
+        VerifyPointsConfig::NoSpec,
         10,
-        5,
         1,
         true,
         true,
-        verification.as_path(),
         synthesis.as_path(),
-        generation.as_path(),
+        verification.as_path(),
         &["x_1_1_0"]);
     let mut main_loop = CEGISLoop::new(config);
 

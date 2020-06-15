87eb5787526c9f6d71586d8e2b7cebbf26d721cc
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-06-15 07:20:09 -0400

    Array examples

---
+++ examples/data/activitysched_sortindex/impl.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+constexpr int N = 3;
+
+std::vector<int> sorti_proxy_impl(int input_0, int input_1, int input_2) {
+   int input[N] = {input_0, input_1, input_2};
+   int ind[N] = {0,0,0};
+   for(int i=0; i<N; ++i) ind[i] = i;
+   int done[N] = {0,0,0};
+   int k=0;
+   for(int i=0; i<N; ++i){
+      for(int j=i+1; j<N; ++j){
+         if( input[ind[j]]< input[ind[i]]){
+            int tmp2 = ind[j];
+            ind[j] = ind[i];
+            ind[i] = tmp2;
+         }
+      }
+   }
+
+   return std::vector<int>(ind, ind+3);
+}
---
+++ examples/data/binarysearch_sort/impl.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+std::vector<int> sort_proxy_impl(int input_0, int input_1, int input_2, int input_3, int input_4) {
+   std::vector<int> output = {input_0,input_1,input_2,input_3,input_4};
+   int k=0;
+   for(int i=0; i<5; ++i){
+      for(int j=i+1; j<5; ++j){
+         if( output[j]< output[i]){
+            int tmp = output[j];
+            output[j] = output[i];
+            output[i] = tmp;
+         }
+      }
+   }
+   return output;
+}

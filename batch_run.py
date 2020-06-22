2cd2dcb7ec3e5a07549602104445f9dfbd9281a2
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-06-22 17:05:38 -0400

    Update batch runner with epoll

--- batch_run.py
+++ batch_run.py
@@ -6,6 +6,8 @@ import subprocess
 import fcntl
 import time
 import errno
+import select
+import tempfile
 
 COMMAND="target/debug/examples/{}"
 DATA_MOD="extract_record"
@@ -27,27 +29,56 @@ def unlock_file(fd):
 
 print_lock = Lock()
 
-def work(target, command, func, data_postfix, log_file):
+def work(target, command, func, data_postfix, log_file_postfix):
+    stdout_log = tempfile.NamedTemporaryFile(suffix=log_file_postfix, prefix="{}.stdout.".format(target), dir=".", delete=False)
+    stderr_log = tempfile.NamedTemporaryFile(suffix=log_file_postfix, prefix="{}.stderr.".format(target), dir=".", delete=False)
     with print_lock:
         print("Running on {}".format(target))
+
     process = subprocess.Popen(command.format(target), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     begin_wall = time.time()
-    process.wait()
+    pollobj = select.epoll()
+    
+    stdout_fd = process.stdout.fileno()
+    stderr_fd = process.stderr.fileno()
+
+    pollobj.register(stdout_fd, select.EPOLLIN | select.EPOLLHUP)
+    pollobj.register(stderr_fd, select.EPOLLIN)
+
+    process_hup = False
+
+    while True:
+        for fd, flags in pollobj.poll():
+            if fd == stdout_fd and (flags & select.EPOLLIN):
+                content = process.stdout.readline()
+                stdout_log.write(content)
+                stdout_log.flush()
+            if fd == stderr_fd and (flags & select.EPOLLIN):
+                content = process.stderr.readline()
+                stderr_log.write(content)
+                stderr_log.flush()
+            if fd == stdout_fd and (flags & select.EPOLLHUP):
+                stdout_log.write(process.stdout.read())
+                stdout_log.flush()
+                stderr_log.write(process.stderr.read())
+                stderr_log.flush()
+                process_hup = True
+        if process_hup:
+            break
+
+    stdout_log.close()
+    stderr_log.close()
+    pollobj.close()
+
     with print_lock:
         print("Finished with {}".format(target))
     wall_time = time.time() - begin_wall
-    stdout_str = process.stdout.read().decode()
-    stderr_str = process.stderr.read().decode()
-    data_line = func(target, stdout_str, stderr_str, wall_time)
-    log_fd = open(log_file, "a")
-    lock_file(log_fd)
-    log_fd.write("Target:{}\n".format(target))
-    log_fd.write("Stdout:\n{}\n".format(stdout_str))
-    log_fd.write("Stderr:\n{}\n".format(stderr_str))
-    unlock_file(log_fd)
-    data_fd = open(target + data_postfix, "a")
-    lock_file(data_fd)
-    data_fd.write(data_line + "\n")
+
+    data_line = func(target, stdout_log.name, stderr_log.name, wall_time)
+    data_fo = open(target + data_postfix, "a")
+    lock_file(data_fo)
+    data_fo.write(data_line + "\n")
+
     with print_lock:
         print("Output for {} finished".format(target))
 
@@ -56,10 +87,10 @@ def main():
     parser.add_option("-n", "--num_job", dest="num_jobs", default=1, type="int", help="Numbers of parallel jobs")
     parser.add_option("-r", "--repeat", dest="repeat", default=1, type="int", help="Repeat time of each job")
     parser.add_option("-c", "--command", dest="command", default=COMMAND, type="string", help="Command to run, subsitute target with '{}'")
-    parser.add_option("-L", "--log_file", dest="log_file", default="run_batch.log", type="string", help="Log file")
+    parser.add_option("-L", "--log_file_postfix", dest="log_file_postfix", default=".log", type="string", help="Log file postfix")
     parser.add_option("-m", "--data_process_module", dest="data_process_module", default=DATA_MOD, type="string", help="Module to look data process function in")
     parser.add_option("-f", "--data_process_func", dest="data_process_func", default=DATA_FUNC, type="string",
-                    help="Data process function name. Expecting signature to be: func(target_name, stdout, stderr, wall_time) -> data(string)")
+                    help="Data process function name. Expecting signature to be: func(target_name, stdout_file_path, stderr_file_path, wall_time) -> data(string)")
     parser.add_option("-D", "--data_postfix", dest="data_postfix", default=".data.csv", type="string", help="Data file postfix")
     (options, args) = parser.parse_args()
 
@@ -67,7 +98,7 @@ def main():
     jobs = args * options.repeat
     pool = ThreadPool(options.num_jobs)
     for j in jobs:
-        pool.apply_async(work, (j, options.command, process_func, options.data_postfix, options.log_file))
+        pool.apply_async(work, (j, options.command, process_func, options.data_postfix, options.log_file_postfix))
     pool.close()
     pool.join()
 
--- extract_record.py
+++ extract_record.py
@@ -1,9 +1,13 @@
 import json
 import re
+import os
 
-def extract(target, stdout_str, stderr_str, wall_time):
+def extract(target, stdout_fn, stderr_fn, wall_time):
+    with open(stdout_fn) as f:
+        stdout_str = f.read()
     res = re.search(r"Record File: (.*\.record\.json)$", stdout_str)
     record = res.group(1)
     with open(record, "r") as f:
         data = json.load(f)
-    return "{},{},{},{}".format(data["solved"], data["wall_time"], data["total_iter"], record)
+    return "{},{},{},{},{},{}".format(data["solved"], data["wall_time"], data["total_iter"],
+        record, os.path.basename(stdout_fn), os.path.basename(stderr_fn))

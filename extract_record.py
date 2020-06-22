023f91dbd2fd5a81eb760c108cb3311bd5ec2c29
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-06-22 05:00:43 -0400

    Add record in csv

--- extract_record.py
+++ extract_record.py
@@ -3,6 +3,7 @@ import re
 
 def extract(target, stdout_str, stderr_str, wall_time):
     res = re.search(r"Record File: (.*\.record\.json)$", stdout_str)
-    with open(res.group(1), "r") as f:
+    record = res.group(1)
+    with open(record, "r") as f:
         data = json.load(f)
-    return "{},{},{}".format(data["solved"], data["wall_time"], data["total_iter"])
+    return "{},{},{}.{}".format(data["solved"], data["wall_time"], data["total_iter"], record)

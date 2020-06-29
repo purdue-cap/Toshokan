f9defd3d9edb7632a24e5809cedf221e033308cc
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-06-29 02:55:18 -0400

    Parallel versions

---
+++ examples/closest_power_two_ilog_p.rs
@@ -0,0 +1,54 @@
+extern crate libpartlibspec;
+use libpartlibspec::cegis::{CEGISConfig, CEGISLoop, VerifyPointsConfig, ExcludedHole};
+use std::path::PathBuf;
+use simplelog::{SimpleLogger, LevelFilter, Config};
+use tempfile::Builder;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut log_level = LevelFilter::Debug;
+    if let Ok(_) = std::env::var("TRACE") {
+        log_level = LevelFilter::Trace;
+    }
+    SimpleLogger::init(log_level, Config::default())?;
+    let base_data_dir = PathBuf::from(file!()).parent().ok_or("Get parent failed")?.join("data/closest_power_two_ilog");
+    let verification = base_data_dir.join("verificationMain.sk");
+    let synthesis = base_data_dir.join("synthesisMainParallel.sk");
+    let impl_file = base_data_dir.join("impl.cpp");
+    let mut sketch_fe_bin = PathBuf::from("sketchsynth");
+    if let Ok(env_path) = std::env::var("SKETCH_FE") {
+        sketch_fe_bin = PathBuf::from(env_path);
+    }
+    let mut sketch_be_bin = PathBuf::from("/usr/share/sketchsynth/bin/cegis");
+    if let Ok(env_path) = std::env::var("SKETCH_BE") {
+        sketch_be_bin = PathBuf::from(env_path);
+    }
+    let mut sketch_home = None;
+    if let Ok(env_path) = std::env::var("SKETCH_HOME") {
+        sketch_home = Some(PathBuf::from(env_path));
+    }
+    let config = CEGISConfig::new(
+        sketch_fe_bin.as_path(),
+        sketch_be_bin.as_path(),
+        sketch_home.as_ref().map(|p| p.as_path()),
+        impl_file.as_path(),
+        "log",
+        "closestTwoPower",
+        1,
+        5,
+        VerifyPointsConfig::NoSpec,
+        10,
+        vec![ExcludedHole::Name("H__0".to_string())].into_iter(),
+        true,
+        true,
+        log_level == LevelFilter::Trace,
+        synthesis.as_path(),
+        verification.as_path(),
+        &["a_0_5_5_0","a_1_6_6_0","a_2_7_7_0","a_3_8_8_0","a_4_9_9_0"], None);
+    let mut main_loop = CEGISLoop::new(config);
+
+    println!("{}", main_loop.run_loop()?.or(Some("Unsolvable benchmark".to_string())).unwrap());
+    let (mut record_file, record_file_path) = Builder::new().prefix("closest_power_two_ilog.").suffix(".record.json").tempfile_in(".")?.keep()?;
+    main_loop.get_recorder().ok_or("Recorder uninitialized")?.write_json_pretty(&mut record_file)?;
+    println!("Record File: {}", record_file_path.file_name().ok_or("No record file name")?.to_str().ok_or("Record file name decode failed")?);
+    Ok(())
+}

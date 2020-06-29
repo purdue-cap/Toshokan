d6a56f36cde8525148a0e3645088512660cc5fb1
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-06-29 06:58:08 -0400

    Parallel versions

---
+++ examples/matexpo_matmult_p.rs
@@ -0,0 +1,57 @@
+extern crate libpartlibspec;
+use libpartlibspec::cegis::{CEGISConfig, CEGISLoop, VerifyPointsConfig, ExcludedHole};
+use std::path::PathBuf;
+use simplelog::{SimpleLogger, LevelFilter, Config};
+use tempfile::Builder;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut log_level = LevelFilter::Debug;
+    if let Ok(_) = std::env::var("TRACE") {
+        log_level = LevelFilter::Trace;
+    }
+    SimpleLogger::init(log_level, Config::default())?;
+    let base_data_dir = PathBuf::from(file!()).parent().ok_or("Get parent failed")?.join("data/matexpo_matmult");
+    let verification = base_data_dir.join("verificationMainParallel.sk");
+    let synthesis = base_data_dir.join("synthesisMain.sk");
+    let impl_file = base_data_dir.join("impl.cpp");
+    let mut sketch_fe_bin = PathBuf::from("sketchsynth");
+    if let Ok(env_path) = std::env::var("SKETCH_FE") {
+        sketch_fe_bin = PathBuf::from(env_path);
+    }
+    let mut sketch_be_bin = PathBuf::from("/usr/share/sketchsynth/bin/cegis");
+    if let Ok(env_path) = std::env::var("SKETCH_BE") {
+        sketch_be_bin = PathBuf::from(env_path);
+    }
+    let mut sketch_home = None;
+    if let Ok(env_path) = std::env::var("SKETCH_HOME") {
+        sketch_home = Some(PathBuf::from(env_path));
+    }
+    let config = CEGISConfig::new(
+        sketch_fe_bin.as_path(),
+        sketch_be_bin.as_path(),
+        sketch_home.as_ref().map(|p| p.as_path()),
+        impl_file.as_path(),
+        "matmul",
+        "main",
+        2,
+        7,
+        VerifyPointsConfig::NoSpec,
+        10,
+        vec![
+            ExcludedHole::Position(20, -1)
+        ].into_iter(),
+        true,
+        true,
+        log_level == LevelFilter::Trace,
+        synthesis.as_path(),
+        verification.as_path(),
+        &["T_0_0_7_13_0", "T_0_1_8_14_0", "T_1_0_9_15_0", "T_1_1_a_16_0",
+        "p_0_b_17_0", "p_1_c_18_0", "p_2_d_19_0"], None);
+    let mut main_loop = CEGISLoop::new(config);
+
+    println!("{}", main_loop.run_loop()?.or(Some("Unsolvable benchmark".to_string())).unwrap());
+    let (mut record_file, record_file_path) = Builder::new().prefix("matexpo_matmult.").suffix(".record.json").tempfile_in(".")?.keep()?;
+    main_loop.get_recorder().ok_or("Recorder uninitialized")?.write_json_pretty(&mut record_file)?;
+    println!("Record File: {}", record_file_path.file_name().ok_or("No record file name")?.to_str().ok_or("Record file name decode failed")?);
+    Ok(())
+}

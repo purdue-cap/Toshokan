b6a0e910370479fcc4090e0924780fc4b6d6360c
Kangjing Huang <huangkangjing@gmail.com>
2020-10-13 01:35:32 -0400

    Proper process management in batch_run

--- batch_run.py
+++ batch_run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from multiprocessing.pool import ThreadPool
 from multiprocessing import Lock, Event
-import os, signal
+import os, signal, sys
+from os import kill
 import optparse
 from select import poll
 import subprocess
@@ -31,15 +32,13 @@ def lock_file(fd):
 def unlock_file(fd):
     fcntl.flock(fd, fcntl.LOCK_UN)
 
-print_lock = Lock()
 
 def work(target, command, func, data_postfix, log_file_postfix, timeout, finish_event):
     stdout_log = tempfile.NamedTemporaryFile(suffix=log_file_postfix, prefix="{}.stdout.".format(target), dir=".", delete=False)
     stderr_log = tempfile.NamedTemporaryFile(suffix=log_file_postfix, prefix="{}.stderr.".format(target), dir=".", delete=False)
-    with print_lock:
-        print("Running on {}".format(target))
+    print("Running on {}".format(target))
 
-    process = subprocess.Popen(command.format(target), preexec_fn=os.setsid, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(command.format(target), preexec_fn=lambda: os.setpgid(0, 0), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     begin_wall = time.time()
     elapsed = 0
     pollobj = select.epoll()
@@ -54,13 +53,16 @@ def work(target, command, func, data_postfix, log_file_postfix, timeout, finish_
     timeouted = False
 
     while True:
-        if finish_event is not None and finish_event.is_set():
-            timeouted = True
-            break
         elapsed = time.time() - begin_wall
+        if finish_event is not None and finish_event.is_set():
+            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+            os.killpg(os.getpgid(process.pid), signal.SIGKILL)
+            process_hup = True
         if timeout > 0 and elapsed > timeout:
             timeouted = True
-            break
+            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+            os.killpg(os.getpgid(process.pid), signal.SIGKILL)
+            process_hup = True
         for fd, flags in pollobj.poll(POLL_INTERNAL):
             if fd == stdout_fd and (flags & select.EPOLLIN):
                 content = process.stdout.readline()
@@ -84,14 +86,13 @@ def work(target, command, func, data_postfix, log_file_postfix, timeout, finish_
     pollobj.close()
 
     if timeouted:
-        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-        os.killpg(os.getpgid(process.pid), signal.SIGKILL)
-        with print_lock:
-            print("Timeout with {} after {} seconds".format(target, elapsed))
-            return
+        print("Timeouted with {} after {} seconds".format(target, elapsed))
+        return
+    if finish_event.is_set():
+        print("Terminated with {} after {} seconds due to a parallel process succeeding".format(target, elapsed))
+        return
 
-    with print_lock:
-        print("Finished with {}".format(target))
+    print("Finished with {}".format(target))
     wall_time = time.time() - begin_wall
 
     data_line = func(target, stdout_log.name, stderr_log.name, wall_time)
@@ -99,13 +100,32 @@ def work(target, command, func, data_postfix, log_file_postfix, timeout, finish_
     lock_file(data_fo)
     data_fo.write(data_line + "\n")
 
-    with print_lock:
-        print("Output for {} finished".format(target))
+    print("Output for {} finished".format(target))
     
     if finish_event is not None:
-        with print_lock:
-            print("Signaling for other threads to timeout")
+        print("Setting completion flag")
         finish_event.set()
+
+# Kills every processes in the current session other than the current one
+def kill_all_other(kill_current_pg = False):
+    session_id = os.getsid(0)
+    current_pid = os.getpid()
+    current_pgid = os.getpgid(0)
+    pids = [int(pid) for pid in os.listdir('/proc') if pid.isdigit()]
+    for pid in pids:
+        if pid == current_pid:
+            continue
+        try:
+            if os.getsid(pid) != session_id:
+                continue
+            if os.getpgid(pid) == current_pgid and kill_current_pg:
+                os.kill(pid, signal.SIGTERM)
+                os.kill(pid, signal.SIGKILL)
+            else:
+                os.killpg(os.getpgid(pid), signal.SIGTERM)
+                os.killpg(os.getpgid(pid), signal.SIGKILL)
+        except ProcessLookupError:
+            continue
 
 def main():
     parser = optparse.OptionParser("Usage: %prog [options] <target>")
@@ -121,33 +141,50 @@ def main():
     parser.add_option("-D", "--data_postfix", dest="data_postfix", default=".data.csv", type="string", help="Data file postfix")
     (options, args) = parser.parse_args()
 
-    process_func = getattr(__import__(options.data_process_module), options.data_process_func)
-    if options.fastest:
-        finish_event = Event()
-        for job in args:
-            with print_lock:
+    pid = os.fork()
+    if pid != 0:
+        signal.signal(signal.SIGINT, lambda _sig, _frame: os.kill(pid, signal.SIGINT))
+        signal.signal(signal.SIGTERM, lambda _sig, _frame: os.kill(pid, signal.SIGTERM))
+        os.waitpid(pid, 0)
+        return
+    os.setsid()
+    def clean_exit(_sig, _frame):
+        print("Cleanup before exit prematruely")
+        kill_all_other(True)
+        sys.exit(1)
+    signal.signal(signal.SIGINT, clean_exit)
+    signal.signal(signal.SIGTERM, clean_exit)
+
+    try:
+        process_func = getattr(__import__(options.data_process_module), options.data_process_func)
+        if options.fastest:
+            finish_event = Event()
+            for job in args:
                 print("Running job {} for fastest".format(job))
-            finish_event.clear()
-            pool = ThreadPool(options.num_jobs)
-            for _ in range(options.num_jobs):
-                pool.apply_async(work, (job, options.command, process_func, options.data_postfix, options.log_file_postfix, 0, finish_event))
-            with print_lock:
+                finish_event.clear()
+                pool = ThreadPool(options.num_jobs)
+                for _ in range(options.num_jobs):
+                    pool.apply_async(work, (job, options.command, process_func, options.data_postfix, options.log_file_postfix, 0, finish_event))
                 print("Waiting for one thread to finish")
-            timeout = None if options.timeout <= 0 else options.timeout
-            if not finish_event.wait(timeout):
-                with print_lock:
+                timeout = None if options.timeout <= 0 else options.timeout
+                if not finish_event.wait(timeout):
                     print("Job {} timeout".format(job))
-                    print("Signaling for all threads to timeout")
-                finish_event.set()
+                    print("Setting completion flag")
+                    finish_event.set()
+                pool.close()
+                print("Killing all remaining processes")
+                kill_all_other()
+                pool.join()
+        else:
+            jobs = args * options.repeat
+            pool = ThreadPool(options.num_jobs)
+            for j in jobs:
+                pool.apply_async(work, (j, options.command, process_func, options.data_postfix, options.log_file_postfix, options.timeout, None))
             pool.close()
             pool.join()
-    else:
-        jobs = args * options.repeat
-        pool = ThreadPool(options.num_jobs)
-        for j in jobs:
-            pool.apply_async(work, (j, options.command, process_func, options.data_postfix, options.log_file_postfix, options.timeout, None))
-        pool.close()
-        pool.join()
+    finally:
+        print("Cleaning up any unkilled processes")
+        kill_all_other(True)
 
 
 if __name__=="__main__":

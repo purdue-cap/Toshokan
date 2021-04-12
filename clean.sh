07809c1852c35d8ef8fb8b147326e9716798a74f
Kangjing Huang <huangkangjing@gmail.com>
2021-04-12 03:11:19 -0400

    Named clean

--- clean.sh
+++ clean.sh
@@ -1,2 +1,7 @@
 #!/bin/bash 
-rm *.log *.json *.csv /tmp/.tmp* /tmp/*.o /tmp/emphermal_record.* /tmp/*.o.tmp -rf
+if [[ -n $1 ]]
+then
+    rm $1*.log $1*.json $1*.csv -f
+else
+    rm *.log *.json *.csv /tmp/.tmp* /tmp/*.o /tmp/emphermal_record.* /tmp/*.o.tmp -rf
+fi

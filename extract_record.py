33054bb5a1ae0ae9ef7f8e1e8926acc3b9b062bb
Kangjing Huang <huangkangjing@gmail.com>
2022-05-03 21:58:50 -0400

    Script changes

--- extract_record.py
+++ extract_record.py
@@ -10,6 +10,6 @@ def extract(target, rtn_code, stdout_fn, stderr_fn, wall_time, env):
     record = res.group(1)
     with open(record, "r") as f:
         data = json.load(f)
-    return ('{},{},{},{},{},{},"{}",{},{},{}'.format(data["solved"], data["total_wall_time"],
-        data["total_synthesis_time"], data["total_verification_time"], data["total_trace_time"],
+    return ('{},{},{},{},{},"{}",{},{},{}'.format(data["solved"], data["total_wall_time"],
+        data["total_synthesis_time"], data["total_verification_time"],
         data["total_iter"], str(env), record, os.path.basename(stdout_fn), os.path.basename(stderr_fn)), data["solved"])
